7a5abee6bc99abe07767c81778f7cd24920b8151
Ilya Kaysin <demarkok@gmail.com>
2017-10-01 02:44:06 +0300

    add Scaladoc

--- Hat/src/main/scala/ru/spbau/kaysin/hat/database/HatStorageActor.scala
+++ Hat/src/main/scala/ru/spbau/kaysin/hat/database/HatStorageActor.scala
@@ -7,17 +7,26 @@ import ru.spbau.kaysin.hat.game.GameActor.Words
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
+/**
+  * An actor representing database which contains all the hats. Each hat contains a list of words which were added to it.
+  */
 class HatStorageActor extends PersistentActor {
 
   import HatStorageActor._
 
+  /**
+    * A map from hat name to list of the words in this hat.
+    */
   val hats: mutable.HashMap[String, ListBuffer[String]] = mutable.HashMap.empty // hatName -> words
 
+  /**
+    * Receive an event (a message modifying database).
+    * @param event The event message.
+    */
   def receiveEvent(event: HatStorageActor.Event): Unit = event match {
       case AddWord(hat, word) =>
         hats.getOrElseUpdate(hat, ListBuffer.empty) += word
   }
-
 
   override def receiveRecover: Receive = {
     case evt: Event => receiveEvent(evt)
@@ -34,9 +43,27 @@ class HatStorageActor extends PersistentActor {
 
 object HatStorageActor {
 
+  /**
+    * Function allowing to create the actor in a safe way.
+    * @return Props object which is passed to system.actorOf
+    */
   def props(): Props = Props(new HatStorageActor())
 
+  /**
+    * A trait representing a Message modifying database
+    */
   trait Event
+
+  /**
+    * Add word into database.
+    * @param hat - the name of the hat in which the word is added
+    * @param word - added word
+    */
   case class AddWord(hat: String, word: String) extends Event
+
+  /**
+    * Get all the words contained in the hat.
+    * @param hat - the name of the hat
+    */
   case class GetWords(hat: String)
 }
