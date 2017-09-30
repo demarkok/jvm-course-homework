87a675a20daedfe7f3464c532e8974f17243f151
Ilya Kaysin <demarkok@gmail.com>
2017-10-01 01:45:37 +0300

    add WithCommand inheritance

--- Hat/src/main/scala/ru/spbau/kaysin/hat/bot/BotState.scala
+++ Hat/src/main/scala/ru/spbau/kaysin/hat/bot/BotState.scala
@@ -3,17 +3,18 @@ package ru.spbau.kaysin.hat.bot
 import akka.actor.ActorRef
 import ru.spbau.kaysin.hat.parser._
 
-sealed trait WithCommands {
+sealed trait BotState {
+  val message: String
+}
+
+
+sealed trait WithCommands extends BotState {
   val commands: List[(Command, String)] // (command, description)
   lazy val buttons: List[String] = commands.map {case (cmd, dsc) => "/" ++ cmd.command ++ " - " ++ dsc}
   val oneTime: Boolean = true
 }
 
-sealed trait BotState {
-  val message: String
-}
-
-case object MainMenu extends BotState with WithCommands {
+case object MainMenu extends WithCommands {
   override val commands = List((AddWords, "Добавить слова"), (Play, "Играть"))
   override val message = "Добавить слова в шляпу или играть?"
 }
@@ -23,15 +24,15 @@ case object HatWaitingAdd extends BotState {
 case object HatWaitingPlay extends BotState {
   override val message = "В какую шляпу?"
 }
-case class Adding(hat: String) extends BotState with WithCommands {
+case class Adding(hat: String) extends WithCommands {
   override val commands = List((EndOfInput, "Закончить ввод"))
   override val message: String = "Добавляйте слова по одному. Когда закончите, пишите " ++ EndOfInput.command
 }
-case class Interlude(game: ActorRef) extends BotState with WithCommands {
+case class Interlude(game: ActorRef) extends WithCommands {
   override val commands = List((StartRound, "Начать раунд"))
   override val message: String = "Введите /" ++ StartRound.command ++ ", чтобы начать раунд."
 }
-case class Round(game: ActorRef) extends BotState with WithCommands{
+case class Round(game: ActorRef) extends WithCommands{
   override val commands = List((NextWord, "Следующее слово"))
   override val message: String = "У вас есть " ++ HatBotActor.roundTime.toString ++ "секунд!"
   override val oneTime = false
